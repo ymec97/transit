9775abe0ba80c94cf5758b8e4db50220a4c0e485
YMeC <ymec97@gmail.com>
2019-12-17 16:55:31 +0200

    Soap: Seperated update field in xml logic to a private function

--- soap_request.py
+++ soap_request.py
@@ -4,6 +4,7 @@ import requests
 import sys
 
 from xml.dom.minidom import parse
+from xml.dom.minidom import Document as XMLDoc
 import xml.dom.minidom
 
 # This consts should be moved (and will be moved) to a better location in the future 
@@ -15,32 +16,47 @@ DEFAULT_REPLY_DEST = "Output/"
 REQUESTS_PATH = "Requests/"
 DEFAULT_REQUEST_FILE = REQUESTS_PATH + "Request_template.xml" # Path to the template file for requests
 NEW_REQUEST_FILE_FORMAT = "{}-{}.xml"
+STOP_FIELD_STRING = "siri:MonitoringRef"
+LINE_FIELD_STRING = "siri:LineRef"
+
+
+def _UpdateField(dom: XMLDoc, field: str, value: str) -> bool:
+	"""
+	Get an xml handler object and update a specific field with a given value
+
+	dom(XMLDoc): Pay attention this is passed by refrence
+	"""
+	try:
+		request = dom.documentElement.getElementsByTagName(field)[0]
+		request_node = request.childNodes[0]
+
+		if request_node.nodeType == request_node.TEXT_NODE:
+			# We reached the actual value to change
+			request_node.replaceWholeText(value)
+
+	except Exception as error_msg:
+		print("Failed updating xml\nError: {error_msg}".format(error_msg=error_msg))
+		return False
+	
+	return True
 
 def _CreateNewRequest(dom, stop_code, line_code):
 	"""
 	load default request file to memory and create a new request file with the new values
 	Return the path to the new request file
 	"""
+
 	new_request_file = "{}{}-{}.xml".format(REQUESTS_PATH, str(line_code), str(stop_code))
-	try:
+	if not _UpdateField(dom, STOP_FIELD_STRING, stop_code):
+		print("Failed updating field [{field}] with value: [{value}]\nError: {error_msg}".format(field=STOP_FIELD_STRING, value=stop_code))
+		return None
+	if not _UpdateField(dom, LINE_FIELD_STRING, line_code):
+		print("Failed updating field [{field}] with value: [{value}]".format(field=LINE_FIELD_STRING, value=line_code))
+		return None
 
-		phase_code = 0
-		collection = dom.documentElement
-		
-		phase_code = 1
-		request = collection.getElementsByTagName("siri:MonitoringRef")[0]
-		request_node = request.childNodes[0]
+	with open(new_request_file, "w") as request_file:
+		dom.writexml(request_file)
 
-		phase_code = 2
-		if request_node.nodeType == request_node.TEXT_NODE:
-			# We reached the actual value to change
-			request_node.replaceWholeText(stop_code)
-
-		with open(new_request_file, "w") as request_file:
-			dom.writexml(request_file)
-
-	except Exception as error_msg:
-		print("Failed parsing ", error_msg)
 
 	# Return the new request file
 	return new_request_file
@@ -56,7 +72,7 @@ def ParseOutput(source_file, format=None):
 def SendRequest(stop_code, line_code):
 	output_file = "{}{}-{}.xml".format(DEFAULT_REPLY_DEST, str(line_code), str(stop_code))
 	
-	DOMTree = xml.dom.minidom.parse(DEFAULT_REQUEST_FILE)
+	DOMTree = parse(DEFAULT_REQUEST_FILE)
 	new_request = _CreateNewRequest(DOMTree, stop_code, line_code)
 	
 	headers = {'content-type': 'text/xml'}
