62fb0d02137935704cb273a22c6cdd38ddf92b14
YMeC <ymec97@gmail.com>
2019-12-18 18:44:40 +0200

    Env: Formatted constants file and added time related consts.

--- env/constants.py
+++ env/constants.py
@@ -1,14 +1,24 @@
 # Not an executable, a simple constants python file
 
-# This consts should be moved (and will be moved) to a better location in the future 
-SIRI_URL="http://siri.motrealtime.co.il:8081/Siri/SiriServices"
-STOP_CODE_ARG = 1
-LINE_CODE_ARG = 2
-MAX_ARGS_NUM = 3
-DEFAULT_REPLY_DEST = "Output/"
-REQUESTS_PATH = "Requests/"
-DEFAULT_REQUEST_FILE = REQUESTS_PATH + "Request_template.xml" # Path to the template file for requests
-NEW_REQUEST_FILE_FORMAT = "{}-{}.xml"
-STOP_FIELD_STRING = "siri:MonitoringRef"
-LINE_FIELD_STRING = "siri:LineRef"
-REQUEST_TIMESTAMP_STRING = "siri:RequestTimestamp"
+# SIRI API related consts
+SIRI_URL 						= "http://siri.motrealtime.co.il:8081/Siri/SiriServices"
+STOP_FIELD_STRING 				= "siri:MonitoringRef"
+LINE_FIELD_STRING 				= "siri:LineRef"
+REQUEST_TIMESTAMP_STRING 		= "siri:RequestTimestamp"
+
+
+# CMD Related consts
+STOP_CODE_ARG 					= 1
+LINE_CODE_ARG 					= 2
+MAX_ARGS_NUM 					= 3
+
+# Environment related consts
+DEFAULT_REPLY_DEST 				= "Output/"
+REQUESTS_PATH 					= "Requests/"
+DEFAULT_REQUEST_FILE 			= REQUESTS_PATH + "Request_template.xml" # Path to the template file for requests
+NEW_REQUEST_FILE_FORMAT 		= "{}-{}.xml"
+
+# Script logic related consts
+DEFAULT_REQUEST_TIME 			= (8, 0, 0) # Default time we would request to view a bus (08:00:00)
+START_TIME_FIELD_STRING 		= "siri:StartTime"
+DEFAULT_GMT_STRING				= "02:00"
